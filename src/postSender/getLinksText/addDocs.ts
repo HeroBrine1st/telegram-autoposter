e381a649dbb507bac15bc85f0ee032f71c215127
n3ktO <ruslannekto@gmail.com>
2021-06-06 15:16:21 +0300

    Add attachment links handler.

---
+++ src/postSender/getLinksText/addDocs.ts
@@ -0,0 +1,15 @@
+import DocType from '../types/mediaTypes/DocType';
+
+function addDocs(docs: DocType[], text: string, linksText: string): string {
+  const nonIncludedDocs = docs.filter(doc => !text.includes(doc.url));
+  if (nonIncludedDocs.length > 0) {
+    linksText += '\n';
+    linksText += '\n<b>🗂 Документы:</b>';
+    for (let doc of nonIncludedDocs) {
+      linksText += `\n<a href="${doc.url}">${doc.title}</a>`;
+    }
+  }
+  return linksText;
+}
+
+export default addDocs;
