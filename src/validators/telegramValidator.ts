8e70be98ff23bd9d4ab457327eace94492a57ce4
n3ktO <ruslannekto@gmail.com>
2021-06-06 18:46:53 +0300

    Edit channel initial handler.

--- src/validators/telegramValidator.ts
+++ src/validators/telegramValidator.ts
@@ -16,7 +16,17 @@ import logger from '../logger';
   }
   
   try {
-    const channel = await bot.getChat(config.get('channel'));
+    let channelIdOrName: any = config.get('channel');
+
+    if (!isNaN(+channelIdOrName)) {
+      channelIdOrName = +channelIdOrName * (channelIdOrName > 0 ? -1 : 1);
+    } else {
+      if (channelIdOrName[0] !== '@') {
+        channelIdOrName = '@' + channelIdOrName;
+      }
+    }
+
+    const channel = await bot.getChat(channelIdOrName);
     config.set('channel', channel.id);
   } catch (e) {
     logger.error('Invalid channel specified or the bot is not a member of this channel.');
